167b3c02db236215ad535606728851a417cdc3ed
Ingila Ejaz <135793481+Ingila185@users.noreply.github.com>
2024-04-10 17:48:08 +0500

    Update TwoSum.js to include hashmap and improve time complexity

--- TwoSum.js
+++ TwoSum.js
@@ -3,17 +3,15 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    arrayOfAns = [];
-    for(var i = 0; i < nums.length; i++)
-    {
-        for(var j = i+1; j < nums.length; j++)
-        if(Number(nums[i] + nums[j])== target)
-        {
-        arrayOfAns.push([i])
-        arrayOfAns.push([j])
-        }
-    }
-
-    return arrayOfAns
+var twoSum = function (nums, target) {
+var prevMap = new Map();
+for (var i = 0; i < nums.length; i++) //Traverse the array. Pick one element.
+{
+    //Calculate difference of that element from target. This would be the complementary pair.
+    let complementaryPair = target - nums[i]; 
+    //See if the Hashmap already contains the complementary pair. If it does, return the index of that element and the current looping variable.
+    if(prevMap.get(complementaryPair) != undefined) return  [prevMap.get(complementaryPair) , i]
+    //Otherwise, add the looping variable with its index to the hashmap. 
+    prevMap.set(nums[i] , i)
+}
 };
